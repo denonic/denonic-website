1027e8327e8d7a3937b0a81de1373c69a157fb3b
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-05-19 00:33:59 -0600

    Created Logo component

--- src/ui/components/Welcome/Welcome.jsx
+++ src/ui/components/Welcome/Welcome.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
+import Logo from "../Logo/Logo";
 
 
 function Welcome() {
     return (
         <div className="welcome">
-            <h1 className="welcome__title">Deno Nicaragua!!!</h1>
+            <Logo/>
+            <div>
+                <h1 className="welcome__title">Deno Nicaragua</h1>
+                <span className="welcome__description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.
+                </span>
+            </div>
         </div>
     );
 }
